d0688aa254672d5100b438411d910c59b1c20021
liammc28 <liam.mcaweeney2@mail.dcu.ie>
2019-01-21 19:14:44 +0000

    Update credentials.ts
    removed config

--- code/src/app/credentials.ts
+++ code/src/app/credentials.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyADKS431yVVCeywX74w1gGPaA0xkJiXSkY",
-  authDomain: "test-project-6c3b1.firebaseapp.com",
-  databaseURL: "https://test-project-6c3b1.firebaseio.com",
-  projectId: "test-project-6c3b1",
-  storageBucket: "test-project-6c3b1.appspot.com",
-  messagingSenderId: "621413137785"
+  
 };
